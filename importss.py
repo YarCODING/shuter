e79b0f7ddcc70c2abb2c0d37ea53b08e39168d80
Yaroslav <20011aric@gmail.com>
2025-02-04 19:17:29 +0200

    enemyboss -> enemy

--- importss.py
+++ importss.py
@@ -2,4 +2,3 @@ from obj.player.script.player import*
 from obj.enemy.script.enemy import*
 from obj.bullet.script.bullet import*
 from obj.enemy_bullet.script.enemy_bullet import*
-from obj.enemyboss.script.enemyboss import*
